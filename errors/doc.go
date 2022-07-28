47d0949ced147dd3e31a43d0812755adcd4494c2
Kristin Jeanna <67802120+kristinjeanna@users.noreply.github.com>
2022-07-28 11:59:24 -0500

    Tweak and expand errors package doc.

--- errors/doc.go
+++ errors/doc.go
@@ -1,2 +1,6 @@
-// Package errors provides custom errors and message formats.
+// Package errors provides general purpose custom errors and error
+// message formats used by the generative package and its subpackages.
+//
+// Custom errors and error message formats that are specific to a given
+// subpackage are defined in the relevant subpackage.
 package errors
